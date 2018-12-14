0f23f5897c62ae5db3eacc482026139645700bc2
Hung Nguyen <anhyeast@yahoo.com>
2018-12-14 15:52:45 -0600

    add responsive

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ class App extends Component {
   render() {
     return (
       <div className={styles.container}>
+        <div className={styles.headerMobile}>
+          <div className={styles.titleOne}>cajun</div>
+          <div className={styles.titleIcon}>
+            <i class="fas fa-warehouse fa-2x" />
+          </div>
+          <div className={styles.titleTwo}>shack</div>
+        </div>
         <div className={styles.headerTop}>
           <div className={styles.iconTop}>
             <i class="fab fa-twitter" />
