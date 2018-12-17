1c21fcf80c5e3e40d8c77943fa16d747363c29fd
hung nguyen <anhyeast@yahoo.com>
2018-12-17 15:42:34 -0600

    nice

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,123 +1,91 @@
 import React, { Component } from "react";
-import styled from "styled-components";
-import Slider from "react-slick";
+import styled, { keyframes } from "styled-components";
+
+const fadeIn = keyframes`
+0% {
+  opacity: 0;
+}
+100% {
+  opacity: 1
+}`;
 
 const Container = styled.div`
   margin: 0;
   padding: 0;
   display: flex;
   flex-direction: column;
-  height: 100vh;
-  width: 100%;
+  position: relative;
+  top: 100px;
 `;
 
 const CanvasMain = styled.div`
-  border: 10px solid #ffc600;
-  margin: 10px;
-  height: 80vh;
-`;
-
-const Canvas = styled.div`
-  border: 10px solid #ffc600;
-  margin: 10px;
+  height: 100vh;
+  margin-top: 10px;
 `;
 
 const Holder = styled.div`
+  position: relative;
   width: 100%;
-  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
 `;
 
 const ImageContainer = styled.img`
-  height: 100%;
-  filter: blur1(px);
+  width: 95%;
+  border-radius: 20px;
+  height: 50%;
+  animation: 2s ${fadeIn} ease-in-out;
 `;
 
 const BGText = styled.div`
   background-color: rgb(0, 0, 0); /* Fallback color */
-  background-color: rgba(0, 0, 0, 0.4); /* Black w/opacity/see-through */
+  background-color: rgba(0, 0, 0, 0.4); /* white w/opacity/see-through */
   color: white;
   font-weight: bold;
-  border: 3px solid #f1f1f1;
+  border: 1px solid #f1f1f1;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   z-index: 2;
-  width: 80%;
-  padding: 20px;
+  height: 80%;
+  width: 70%;
+  overflow: hidden;
   text-align: center;
 `;
 
 export default class Main extends Component {
   render() {
-    var settings = {
-      dots: false,
-      fade: true,
-      autoplay: true,
-      arrows: false,
-      infinite: true,
-      speed: 3000,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      initialSlide: 0,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            dots: false,
-            fade: true,
-            autoplay: true,
-            arrows: false,
-            infinite: true,
-            speed: 3000
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2,
-            dots: false,
-            fade: true,
-            autoplay: true,
-            arrows: false,
-            infinite: true,
-            speed: 2000
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: false,
-            fade: true,
-            autoplay: true,
-            arrows: false,
-            infinite: true,
-            speed: 3000
-          }
-        }
-      ]
-    };
     return (
       <Container>
         <CanvasMain>
-          <Slider {...settings}>
-            <Holder>
-              <ImageContainer src="/images/image1.jpg" />
-              <BGText>best poboys</BGText>
-            </Holder>
-            <Holder>
-              <ImageContainer src="/images/image2.jpg" />
-              <BGText>amazing seafoods</BGText>
-            </Holder>
-          </Slider>
+          <Holder>
+            <ImageContainer src="/images/image1.jpg" />
+            <BGText>
+              <h2>Poboys</h2>
+              <p>
+                <h6>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit repudiandae corrupti accusamus
+                  provident ab quos totam. Nostrum adipisci illo nam.
+                </h6>
+              </p>
+            </BGText>
+          </Holder>
+          <Holder>
+            <ImageContainer src="/images/image2.jpg" />
+            <BGText>
+              <h2>Amazing Seafood</h2>
+              <p>
+                <h6>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id nisi nostrum ab laboriosam vitae saepe
+                  eos debitis aperiam aspernatur nemo!
+                </h6>
+              </p>
+            </BGText>
+          </Holder>
         </CanvasMain>
-        <Canvas>item2</Canvas>
       </Container>
     );
   }
